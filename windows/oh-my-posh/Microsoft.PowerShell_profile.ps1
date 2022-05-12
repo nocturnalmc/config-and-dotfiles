6e3cc56e8db57a0fac28799c7e395dd1e7e58a7b
noct <ezy_95@outlook.com>
2022-05-12 08:03:58 +0800

    oh-my-posh change to powerlevel10k_rainbow

--- windows/oh-my-posh/Microsoft.PowerShell_profile.ps1
+++ windows/oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
 oh-my-posh init pwsh | Invoke-Expression
-oh-my-posh init pwsh --config "$(scoop prefix oh-my-posh)\themes\jandedobbeleer.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$(scoop prefix oh-my-posh)\themes\powerlevel10k_rainbow.omp.json" | Invoke-Expression
