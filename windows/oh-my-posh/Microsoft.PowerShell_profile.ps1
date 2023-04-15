20d0eb14753e6b17e7893e9134846b230eca5178
noct <ezy_95@outlook.com>
2023-04-16 00:10:23 +0800

    fix: update PowerShell for oh-my-posh init following current docs

--- windows/oh-my-posh/Microsoft.PowerShell_profile.ps1
+++ windows/oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
-# Create file at $Home\[My ]Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+# Create file at $Home\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 oh-my-posh init pwsh | Invoke-Expression
-oh-my-posh init pwsh --config "$(scoop prefix oh-my-posh)\themes\powerlevel10k_rainbow.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json" | Invoke-Expression
+
+cd E:\Development
